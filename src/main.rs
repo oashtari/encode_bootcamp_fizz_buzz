03b7dcf9b9462d4e872dd66211b6e52dbd9fe647
omidashtari <oashtari@gmail.com>
2022-10-28 18:27:20 -0700

    small edit to eliminate some print lines

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,22 @@ fn main() {
 fn fizz_buzz(number: i16) {
     let mut count = 1;
     let mut fb_count = 0;
-    let mut fizz_count = 0;
-    let mut buzz_count = 0;
+    // let mut fizz_count = 0;
+    // let mut buzz_count = 0;
     while count <= number {
         if count % 3 == 0 && count % 5 == 0 {
             println!("fizz buzz");
             fb_count += 1;
         } else if count % 3 == 0 {
             println!("fizz");
-            fizz_count += 1;
+            // fizz_count += 1;
         } else if count % 5 == 0 {
             println!("buzz");
-            buzz_count += 1;
+            // buzz_count += 1;
         }
         count += 1;
     }
     println!("fizz buzz count: {} ", fb_count);
-    println!("fizz  count: {} ", fizz_count);
-    println!("buzz count: {} ", buzz_count);
+    // println!("fizz  count: {} ", fizz_count);
+    // println!("buzz count: {} ", buzz_count);
 }
